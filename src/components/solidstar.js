90cbb7cb3d4cdc76c2476723c43592cfeacb1616
fadilayoga <fadilayoga@users.noreply.github.com>
2021-10-27 20:49:41 +0700

    add dashboard and detail

---
+++ src/components/solidstar.js
@@ -0,0 +1,10 @@
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+function Solidstar(){
+    return(
+        <FontAwesomeIcon icon={faStar} size="lg" />
+    )
+}
+
+export default Solidstar
---
+++ src/pages/dashboard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from '../components/header'
+import Promo from '../components/promo'
+import Items from '../components/items'
+import { Stack } from 'react-bootstrap'
+
+function Dashboard() {
+    return (
+        <>
+            <Header />
+            <Stack gap={4}>
+                <Promo />
+                <Items />
+            </Stack>
+        </>
+    )
+}
+
+export default Dashboard
